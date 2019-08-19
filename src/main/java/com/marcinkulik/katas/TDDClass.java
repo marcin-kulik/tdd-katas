bd7352250fc2ffc292a4b247f325157eeabd2bce
Marcin Kulik <marcinkulik@icloud.com>
2019-08-19 16:55:53 +0100

    Updating package name to be same on each branch.

---
+++ src/main/java/com/marcinkulik/katas/TDDClass.java
@@ -0,0 +1,5 @@
+package com.marcinkulik.katas;
+
+public class TDDClass {
+
+}
---
+++ src/test/java/com/marcinkulik/katas/Test.java
@@ -0,0 +1,12 @@
+package com.marcinkulik.katas;
+
+import static org.junit.Assert.*;
+
+public class Test {
+
+    @org.junit.Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
